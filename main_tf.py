aaabf2e13af6811b715cf6e098ecf87d7140296a
ABoghi <a.boghi@hotmail.it>
2020-04-20 22:35:02 +0100

    preprocess dataset.

--- main_tf.py
+++ main_tf.py
@@ -6,11 +6,6 @@ import tensorflow as tf
 titanic_train = 'https://storage.googleapis.com/tf-datasets/titanic/train.csv'
 titanic_eval = 'https://storage.googleapis.com/tf-datasets/titanic/eval.csv'
 
-'''dftrain = pd.read_csv() # training data
-dfeval = pd.read_csv() # testing data
-y_train = dftrain.pop('survived')
-y_eval = dfeval.pop('survived')'''
-
 
 def analyse_input(training_input: str = titanic_train):
 
@@ -33,12 +28,36 @@ def analyse_input(training_input: str = titanic_train):
     return training_df
 
 
-def run_ml_with_tf(output_field: str, training_input: str = titanic_train, evaluation_input: str = titanic_eval):
+def preprocess_dataset(output_field: str, training_input: str = titanic_train):
 
     training_df = pd.read_csv(training_input)
-    evaluation_df = pd.read_csv(evaluation_input)
 
     output_training = training_df.pop(output_field)
-    output_evaluation = evaluation_df.pop(output_field)
 
-    return
+    CATEGORICAL_COLUMNS = []
+    NUMERIC_COLUMNS = []
+    for cols in training_df.columns:
+        if training_df[cols].dtypes == object:
+            CATEGORICAL_COLUMNS.append(cols)
+        else:
+            NUMERIC_COLUMNS.append(cols)
+
+    # Creating a list of features used in the dataset.
+    # tf.feature_column. create an object that the model can
+    # use to map string values to integers, avoiding to manually
+    # having to encode the dataframes.
+
+    feature_columns = []
+    for feature_name in CATEGORICAL_COLUMNS:
+        # gets a list of all unique values from given feature column
+        vocabulary = training_df[feature_name].unique()
+        feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(
+            feature_name, vocabulary))
+
+    for feature_name in NUMERIC_COLUMNS:
+        feature_columns.append(tf.feature_column.numeric_column(
+            feature_name, dtype=tf.float32))
+
+    print(feature_columns)
+
+    return training_df, output_training, feature_columns

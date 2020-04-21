63b829b0c99ddf46275cf6f01b062839dfc5c500
ABoghi <a.boghi@hotmail.it>
2020-04-21 18:59:55 +0100

    Again.

--- main_tf.py
+++ main_tf.py
@@ -7,37 +7,56 @@ titanic_train = 'https://storage.googleapis.com/tf-datasets/titanic/train.csv'
 titanic_eval = 'https://storage.googleapis.com/tf-datasets/titanic/eval.csv'
 
 
-def analyse_input(training_input: str = titanic_train):
+def analyse_input(input_file: str = titanic_train):
+    """
+    Parameters
+    ----------
+    input_file: str
 
-    training_df = pd.read_csv(training_input)
+    """
+
+    input_df = pd.read_csv(input_file)
     print(f"Training Set:")
-    print(training_df.head())
+    print(input_df.head())
     print()
     print(f"Statstics on Training Set:")
-    print(training_df.describe())
+    print(input_df.describe())
 
-    for cols in training_df.columns:
-        if training_df[cols].dtypes == object:
+    for cols in input_df.columns:
+        if input_df[cols].dtypes == object:
             figo = plt.figure()
-            training_df[cols].value_counts().plot(kind='barh')
+            input_df[cols].value_counts().plot(kind='barh')
         else:
             fign = plt.figure()
-            training_df[cols].hist()
+            input_df[cols].hist()
             plt.ylabel(cols)
 
-    return training_df
+    return input_df
 
 
-def preprocess_dataset(output_field: str, training_input: str = titanic_train):
+def preprocess_dataset(output_field: str, input_file: str = titanic_train):
+    """
+    Parameters
+    ----------
+    output_field: str
+    input_file: str
 
-    training_df = pd.read_csv(training_input)
 
-    output_training = training_df.pop(output_field)
+    Returns
+    -------
+    input_df
+    output_training
+    feature_columns
+    """
+
+    input_df = pd.read_csv(input_file)
+
+    output_training = input_df.pop(output_field)
 
     CATEGORICAL_COLUMNS = []
     NUMERIC_COLUMNS = []
-    for cols in training_df.columns:
-        if training_df[cols].dtypes == object:
+    for cols in input_df.columns:
+        if input_df[cols].dtypes == object:
             CATEGORICAL_COLUMNS.append(cols)
         else:
             NUMERIC_COLUMNS.append(cols)
@@ -50,7 +69,7 @@ def preprocess_dataset(output_field: str, training_input: str = titanic_train):
     feature_columns = []
     for feature_name in CATEGORICAL_COLUMNS:
         # gets a list of all unique values from given feature column
-        vocabulary = training_df[feature_name].unique()
+        vocabulary = input_df[feature_name].unique()
         feature_columns.append(tf.feature_column.categorical_column_with_vocabulary_list(
             feature_name, vocabulary))
 
@@ -60,4 +79,66 @@ def preprocess_dataset(output_field: str, training_input: str = titanic_train):
 
     print(feature_columns)
 
-    return training_df, output_training, feature_columns
+    return input_df, output_training, feature_columns
+
+
+def make_input_function(data_df: pd.core.frame.DataFrame, label_df: pd.core.series.Series, num_epochs: int = 10, shuffle: bool = True, batch_size: int = 32):
+    """
+    Parameters
+    ----------
+    data_df: pd.core.frame.DataFrame
+    label_df: pd.core.series.Series
+    num_epochs: int
+    shuffle: bool
+    batch_size: int
+
+    Returns
+    -------
+    input_function: function
+        returns a function object for use.
+
+    """
+
+    def input_function():  # inner function, this will be returned
+        # create tf.data.Dataset object with data and its label
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df))
+        if shuffle:
+            ds = ds.shuffle(1000)  # randomize order of data
+        # split dataset into batches of 32 and repeat process for number of epochs
+        ds = ds.batch(batch_size).repeat(num_epochs)
+        return ds  # return a batch of the dataset
+
+    return input_function  # return a function object for use
+
+
+def train_linear_estimator(output_field: str = 'survived', training_file: str = titanic_train, num_epochs: int = 10, shuffle: bool = True, batch_size: int = 32):
+    """
+    Parameters
+    ----------
+    output_field: str
+    training_file: str
+    num_epochs: int 
+    shuffle: bool
+    batch_size: int
+
+    Returns
+    -------
+    trained_linear_estimator
+
+    """
+
+    # preprocess the dataset
+    training_df, output_training, feature_columns = preprocess_dataset(
+        output_field, training_file)
+
+    # make the training function
+    training_function = make_input_function(
+        training_df, output_training, num_epochs, shuffle, batch_size)
+
+    # linear estimator
+    linear_estimator = tf.estimator.LinearClassifier(
+        feature_columns=feature_columns)
+
+    trained_linear_estimator = linear_estimator.train(training_function)
+
+    return trained_linear_estimator
